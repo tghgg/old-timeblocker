f28740524be938ee94558a18d9e3bf02dd25f373
Choppa2 <falling_snowdin@protonmail.com>
2020-01-30 14:12:59 +0700

    Add ability to delete blocks.

--- main.js
+++ main.js
@@ -57,10 +57,36 @@ app.on('ready', () => {
   // mainWindow.webContents.openDevTools();
 });
 
-// Time block creator dialog
-ipcMain.on('show_block', (input) => {
+// Show block info box
+// Reminder that 'on' methods have two arguments: event, data
+ipcMain.on('show_block', (event, input) => {
   dialog.showMessageBox(mainWindow, {
     message: 'Block Name: ' + input,
-    buttons: ['OK']
+    // Have buttons for edit, delete, or do nothing options
+    // showMessageBox returns a Promise with the index of the button the user clicked on
+    buttons: ['OK', 'Delete', 'Edit']
+  }).then((response) => {
+    console.log(response.response + ' was the index of the button clicked.');
+    switch(response.response) {
+      case 0:
+        console.log('Close block info box.');
+        break;
+      case 1: 
+        // Send back to info of the about-to-be-deleted block
+        mainWindow.webContents.send('delete_block', input);
+        console.log('Delete this block');
+        break;
+      case 2:
+        // Send back to info of the about-to-be-edited block
+        mainWindow.webContents.send('edit_block', input);
+        console.log('Edit this block.');
+        break;
+      default:
+        console.log('Something went wrong trying to close the block info dialog box!');
+        break;
+    }
+  }, (err) => {
+    console.log(err);
+    console.log("Error retrieving a response from the block info dialog box!");
   });
 });
